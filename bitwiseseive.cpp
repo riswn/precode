870bf5bc0a4702d79db70b0a9ac9abf52c4d1583
riswn <48291526+riswn@users.noreply.github.com>
2020-04-30 21:19:13 +0600

    Update bitwiseseive.cpp

--- bitwiseseive.cpp
+++ bitwiseseive.cpp
@@ -1,27 +1,46 @@
-#define MAX 100000000//max num to prime
-#define LMT 10000 //sqrt(MAX)
- 
-int flag[MAX/64];
-int prime[100000000],total;
- 
-#define chkC(n) (flag[n>>6]&(1<<((n>>1)&31)))
-#define setC(n) (flag[n>>6]|=(1<<((n>>1)&31)))
- 
+define MAX 46656
+#define LMT 216
+#define LEN 4830
+#define RNG 100032
+
+unsigned base[MAX/64], segment[RNG/64], primes[LEN];
+
+#define sq(x) ((x)*(x))
+#define mset(x,v) memset(x,v,sizeof(x))
+#define chkC(x,n) (x[n>>6]&(1<<((n>>1)&31)))
+#define setC(x,n) (x[n>>6]|=(1<<((n>>1)&31)))
+
+/* Generates all the necessary prime numbers and marks them in base[]*/
 void sieve()
 {
-    unsigned i,j,k;
- 
-    flag[0]|=0;
-    for(i=3;i<LMT;i+=2)
-        if(!chkC(i))
-            for(j=i*i,k=i<<1;j<MAX;j+=k)
-                setC(j);
- 
-    prime[(j=0)++] = 2;//prime starts from 0 position
-    for(i=3;i<MAX;i+=2)
-        if(!chkC(i))
-        {
-            prime[j++] = i;
-            //printf("%d\n",primes[j-1]);
-        }
-    total = j-1;
+    unsigned i, j, k;
+    for(i=3; i<LMT; i+=2)
+        if(!chkC(base, i))
+            for(j=i*i, k=i<<1; j<MAX; j+=k)
+                setC(base, j);
+    for(i=3, j=0; i<MAX; i+=2)
+        if(!chkC(base, i))
+            primes[j++] = i;
+}
+
+/* Returns the prime-count within range [a,b] and marks them in segment[] */
+int segmented_sieve(int a, int b)
+{
+    unsigned i, j, k, cnt = (a<=2 && 2<=b)? 1 : 0;
+    if(b<2) return 0;
+    if(a<3) a = 3;
+    if(a%2==0) a++;
+    mset(segment,0);
+    for(i=0; sq(primes[i])<=b; i++)
+    {
+        j = primes[i] * ( (a+primes[i]-1) / primes[i] );
+        if(j%2==0) j += primes[i];
+        for(k=primes[i]<<1; j<=b; j+=k)
+            if(j!=primes[i])
+                setC(segment, (j-a));
+    }
+    for(i=0; i<=b-a; i+=2)
+        if(!chkC(segment, i))
+            cnt++;
+    return cnt;
+}
